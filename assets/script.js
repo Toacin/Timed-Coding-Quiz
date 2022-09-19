ceb95b281bd7804f995899f59c8baeed09952e2b
Toacin <toacinp@outlook.com>
2022-09-18 23:27:47 -0700

    added function to check if answer is correct or incorrect

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,8 @@ const $choi1 = document.querySelector("#choi1");
 const $choi2 = document.querySelector("#choi2");
 const $choi3 = document.querySelector("#choi3");
 const $choi4 = document.querySelector("#choi4");
-const $nextButton = document.querySelector("#next-button")
+const $nextButton = document.querySelector("#next-button");
+const $quizResponse = document.querySelector("#quizResponse");
 const questions = ["All of the following are semantic HTML container elements except?",
 "CSS selectors include all of the following except?",
 "Which of the following is a property of an array in Javascript?",
@@ -22,10 +23,10 @@ const answerChoices = [["Section","Article","Aside","Img"],
 ["length","concat","slice","unshift"],
 ["number","boolean","string","undefined"],
 ["row","row-reverse","column","column-reverse"]];
-let secondsLeft = 10;
+let secondsLeft = 30;
 
 function setTimer() {
-    $timer.textContent = `Time Left: 10 seconds left`;
+    $timer.textContent = `Time Left: 30 seconds left`;
     let timerInterval = setInterval(function() {
         secondsLeft = secondsLeft - 1;
         $timer.textContent = `Time Left: ${secondsLeft} seconds left`;
@@ -41,29 +42,38 @@ function setTimer() {
     }, 1000);
 }
 
-let x = 0;
+let scrollIndex = 0;
 function questionScroller () {
-    if (x<4) {
-        $question.textContent = questions[x];
-        $choi1.textContent = (answerChoices[x][0]);
-        $choi2.textContent = (answerChoices[x][1]);
-        $choi3.textContent = (answerChoices[x][2]);
-        $choi4.textContent = (answerChoices[x][3]);
-        $choice1.value = answerChoices[x][0];
-        $choice2.value = answerChoices[x][1];
-        $choice3.value = answerChoices[x][2];
-        $choice4.value = answerChoices[x][3];
-        x++;
-        document.getElementById("choice1").checked = false;
-        document.getElementById("choice2").checked = false;
-        document.getElementById("choice3").checked = false;
-        document.getElementById("choice4").checked = false;
+    if (scrollIndex<4) {
+        $quizResponse.style.display = "none";
+        $question.textContent = questions[scrollIndex];
+        $choi1.textContent = (answerChoices[scrollIndex][0]);
+        $choi2.textContent = (answerChoices[scrollIndex][1]);
+        $choi3.textContent = (answerChoices[scrollIndex][2]);
+        $choi4.textContent = (answerChoices[scrollIndex][3]);
+        $choice1.value = answerChoices[scrollIndex][0];
+        $choice2.value = answerChoices[scrollIndex][1];
+        $choice3.value = answerChoices[scrollIndex][2];
+        $choice4.value = answerChoices[scrollIndex][3];
+        scrollIndex++;
+        $choice1.checked = false;
+        $choice2.checked = false;
+        $choice3.checked = false;
+        $choice4.checked = false;
     }
-
-    // if(x>0) {
-    //     document.querySelector('input[name="question"]:checked').checked = false;
-    // }
 }
+
+function answerChecker (e) {
+    if (e.currentTarget.value == "Img" || e.currentTarget.value == "^" || e.currentTarget.value == "length" || e.currentTarget.value == "string" || e.currentTarget.value == "row") {
+        $quizResponse.textContent = "That's correct!";
+        $quizResponse.style.display = "block";
+        $quizResponse.style.color = "green";
+    } else {
+        $quizResponse.textContent = "That is incorrect";
+        $quizResponse.style.display = "block";
+        $quizResponse.style.color = "red";
+    }
+};
 
 $startbutton.addEventListener("click", function() {
     $startingMessage.style.display = "none";
@@ -76,3 +86,9 @@ $nextButton.addEventListener("click", function(e) {
     e.preventDefault();
     questionScroller();
 });
+
+$choice1.addEventListener("click", answerChecker)
+$choice2.addEventListener("click", answerChecker)
+$choice3.addEventListener("click", answerChecker)
+$choice4.addEventListener("click", answerChecker)
+
