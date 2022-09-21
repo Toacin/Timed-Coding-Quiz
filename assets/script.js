99221ec5c8f3c86edc48d7c36e0ec56e72773f7f
Toacin <toacinp@outlook.com>
2022-09-20 21:53:48 -0700

    added message when user tries to continue to next question without picking an answer first. There is also different alerts depending on if user enters less than or more than two characters for initials.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// HTML elements grabbed with query selector set to variables below
 const $startbutton = document.querySelector("#starting-button");
 const $startingHeader = document.querySelector("#startingHeader");
 const $startingMessage = document.querySelector("#startingMessage");
@@ -14,6 +15,7 @@ const $choi3 = document.querySelector("#choi3");
 const $choi4 = document.querySelector("#choi4");
 const $nextButton = document.querySelector("#next-button");
 const $quizResponse = document.querySelector("#quizResponse");
+// library of 5 questions and its respective answer choice sets declared in arrays below
 const questions = ["All of the following are semantic HTML container elements except?",
 "CSS selectors include all of the following except?",
 "Which of the following is a property of an array in Javascript?",
@@ -25,16 +27,19 @@ const answerChoices = [["Section","Article","Aside","Img"],
 ["number","boolean","string","undefined"],
 ["row","row-reverse","column","column-reverse"]];
 
-let secondsLeft = 30;
+// timer function below
+let secondsLeft = 60;
 function setTimer() {
-    $timer.textContent = `Time Left: 30 seconds left`;
+    $timer.textContent = `Time Left: 60 seconds left`;
     let timerInterval = setInterval(function() {
         secondsLeft = secondsLeft - 1;
         $timer.textContent = `Time Left: ${secondsLeft} seconds left`;
 
+        // timer turns color red at 5 seconds
         if(secondsLeft <= 5) {
             $timer.style.color = "red";
         }
+        // timer changes starting splash screen to try again if user runs out of time
         if(secondsLeft <= 0) {
             clearInterval(timerInterval);
             $timer.textContent = `Time Left: 0 seconds left`;
@@ -43,41 +48,50 @@ function setTimer() {
             $startingHeader.textContent = "Time's Up";
             document.querySelector("#info").textContent = "Click START button to try again!"
             $startingMessage.style.display = "inherit";
-            secondsLeft = 30;
+            secondsLeft = 60;
             $timer.style.color = "grey";
         }
+        // clear timer if user reaches last question
         if(scrollIndex === 6) {
             clearInterval(timerInterval);
             $timer.textContent = "";
         }
+        // in case user decides to navigate to high score leaderboard, this will stop timer
         document.querySelector("#leaderboard").addEventListener("click", function() {
             clearInterval(timerInterval);
         })
     }, 1000);
 }
 
+// function to scroll through questions
 let scrollIndex = 0;
 let finalScore = 0;
 function questionScroller () {
     if (scrollIndex<5) {
         $quizResponse.style.display = "none";
+        // scroll to next question based on current index value
         $question.textContent = questions[scrollIndex];
+        // iterate answer choices based on current index value
         $choi1.textContent = (answerChoices[scrollIndex][0]);
         $choi2.textContent = (answerChoices[scrollIndex][1]);
         $choi3.textContent = (answerChoices[scrollIndex][2]);
         $choi4.textContent = (answerChoices[scrollIndex][3]);
+        // change input's value to it's respective answer choice (used in later function)
         $choice1.value = answerChoices[scrollIndex][0];
         $choice2.value = answerChoices[scrollIndex][1];
         $choice3.value = answerChoices[scrollIndex][2];
         $choice4.value = answerChoices[scrollIndex][3];
+        // changes "next" button to "submit" button when it's the last question 
         if (scrollIndex === 4) {
             $nextButton.textContent = "Submit";
         }
         scrollIndex++;
+        // uncheck selected input from previous question
         $choice1.checked = false;
         $choice2.checked = false;
         $choice3.checked = false;
         $choice4.checked = false;
+    // once user finishes quiz, calculate final score. In the event of negative score, change to 0. then run score screen function
     } else if (scrollIndex === 5) {
         finalScore = secondsLeft;
         if (finalScore < 0) {
@@ -88,6 +102,7 @@ function questionScroller () {
     }
 }
 
+//score screen function brings up final score display and asks user to submit initials
 function scoreScreen() {
     $questionForm.style.display = "none";
     $timer.style.display = "none";
@@ -112,11 +127,18 @@ function scoreScreen() {
     $hsSection.appendChild($hsFormSubmit);
     $startingMessage.appendChild($hsSection);
 
+    // on click of submit button, this anonymous callback function will run
     $hsFormSubmit.addEventListener("click", function() {
+        // this if statement checks if anything other than 2 characters were entered for initials. If so, it will alert user to try again with only 2 characters
         if ($hsInput.value.trim().length !== 2) {
-            alert("Please enter 2 characters for initials");
+            if ($hsInput.value.trim().length < 2) {
+                alert("Please enter at least 2 characters for initials");
+            } else {
+                alert("Please enter only 2 characters for initials")
+            }
             return;
         } else {
+            // this else statement will ensure only characters from alphabet were entered
             for (i=0; i<$hsInput.value.trim().length; i++) {
                 if (["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"].includes($hsInput.value.trim().toUpperCase().split("")[i])) {
                 } else {
@@ -125,35 +147,43 @@ function scoreScreen() {
                 }
             }
 
+            // initials and score saved in object
             savedScore = {
                 initials: $hsInput.value.trim().toUpperCase(),
                 highScoreEntry: finalScore
             };
+            //retrieve library of highscore from local storage if exist. otherwise create one
             if (JSON.parse(localStorage.getItem("scores-list"))) {
                 highScoreLib = JSON.parse(localStorage.getItem("scores-list"));
             } else {
                 highScoreLib = [];
             }
+            // update retrieved library of scores
             highScoreLib.push(savedScore);
+            // this console log is for me to ensure library is indeed updated
             console.log(highScoreLib);
+            // set new library of scores in local storage and run highscore function
             localStorage.setItem("scores-list", JSON.stringify(highScoreLib));
             highScore();
         }
     })
 };
 
+// goes to highscore screen. Turns off previously displaying screen and timer
 function highScore() {
     $timer.style.display = "none";
     $startingMessage.style.display = "none";
     $questionForm.style.display = "none";
     document.querySelector("#highscore").style.display = "flex";
+    // retrieve data from local storage if exists, otherwise create empty array
     if (JSON.parse(localStorage.getItem("scores-list"))) {
         localHighScoreLib = JSON.parse(localStorage.getItem("scores-list"));
     } else {
         localHighScoreLib = [];
         document.querySelector("#scoresList").innerHTML = "";
     }
-
+    
+    // append scores if previous scores exist in local storage
     for (i=0; i<localHighScoreLib.length; i++) {
         let eachUser = localHighScoreLib[i];
         let $newEntry = document.createElement("li");
@@ -161,18 +191,17 @@ function highScore() {
         document.querySelector("#scoresList").appendChild($newEntry);
     }
 
+    // on click of clear, clear local storage key holding scores library, and recall highscore function to clear high scores
     document.querySelector("#clear").addEventListener("click", function() {
         localStorage.removeItem("scores-list");
         highScore();
 
     })
-    document.querySelector("#home").addEventListener("click", function() {
-        document.querySelector("#highscore").style.display = "none";
-        init();
-    })
 }
 
+// function checks answers
 function answerChecker (e) {
+    // if input is one of the following values, answer is correct
     if (e.target.value == "Img" || e.target.value == "^" || e.target.value == "length" || e.target.value == "string" || e.target.value == "row") {
         $quizResponse.textContent = "That's correct!";
         $quizResponse.style.display = "block";
@@ -181,10 +210,12 @@ function answerChecker (e) {
         $quizResponse.textContent = "That is incorrect";
         $quizResponse.style.display = "block";
         $quizResponse.style.color = "red";
-        secondsLeft = secondsLeft - 4;
+        // reduct time if answer is wrong
+        secondsLeft = secondsLeft - 8;
     }
 };
 
+// once start button is clicked, start questions and timer
 $startbutton.addEventListener("click", function() {
     $startingMessage.style.display = "none";
     $questionForm.style.display = "block";
@@ -192,19 +223,21 @@ $startbutton.addEventListener("click", function() {
     setTimer();
 });
 
+// once next or submit button is clicked, move to next function
 $nextButton.addEventListener("click", function(e) {
     e.preventDefault();
+    // if no answer is chosen, ask user to first pick an answer before continuing
     if ($choice1.checked === false && $choice2.checked === false && $choice3.checked === false && $choice4.checked === false) {
+        $quizResponse.textContent = "Please choose an answer before continuing";
+        $quizResponse.style.display = "block";
+        $quizResponse.style.color = "red";
         return;
     }
+    // question scroller function called to move to next question
     questionScroller();
 });
 
-// $choice1.addEventListener("click", answerChecker);
-// $choice2.addEventListener("click", answerChecker);
-// $choice3.addEventListener("click", answerChecker);
-// $choice4.addEventListener("click", answerChecker);
-
+// if an input is chosen, check it's answer with answerchecker function
 $questionForm.addEventListener("click", function(e) {
     if (e.target.matches("input")) {
         answerChecker(e);
@@ -212,7 +245,9 @@ $questionForm.addEventListener("click", function(e) {
     console.log(e);
 });
 
+// call highscore function if highscore link in nav is clicked
 document.querySelector("#leaderboard").addEventListener("click", function() {
+    // will ensure user isn't already in highscore screen before calling function. otherwise, highscore function will re-append scores from local storage each time link is clicked 
     if (document.querySelector("#highscore").style.display !== "flex") {
         highScore();
     }
